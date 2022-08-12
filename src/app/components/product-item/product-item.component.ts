4f2c768a8c6d58bff84ddc90ed6b7a79f4aefbc8
Tim <Tim280696@hotmail.com>
2022-08-12 12:36:59 +0200

    product-item: create component

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.css']
+  styleUrls: ['./product-item.component.css'],
 })
 export class ProductItemComponent implements OnInit {
+  @Input() product: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
