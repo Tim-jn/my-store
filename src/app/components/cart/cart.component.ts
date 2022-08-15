83af6c628e718bed01de426d628388116aec1b74
Tim <Tim280696@hotmail.com>
2022-08-15 18:19:36 +0200

    Create cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  products = this.cartService.getItems();
+  total: number = 0;
 
-  constructor() { }
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
+    this.products.map((product) => {
+      this.total += product.price;
+    });
   }
-
 }
