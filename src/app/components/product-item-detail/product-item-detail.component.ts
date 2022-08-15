10bbed4c302433ca5b5562d8538dde796c6e44b9
Tim <Tim280696@hotmail.com>
2022-08-15 15:49:39 +0200

    Create component and add dynamic routing

--- src/app/components/product-item-detail/product-item-detail.component.ts
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { productProps } from '../product-list/product-list.component';
+import { ProductService } from 'src/app/services/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-item-detail',
   templateUrl: './product-item-detail.component.html',
-  styleUrls: ['./product-item-detail.component.css']
+  styleUrls: ['./product-item-detail.component.css'],
 })
 export class ProductItemDetailComponent implements OnInit {
+  product!: productProps;
+  id!: number;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.route.params.subscribe((params) => {
+      this.id = parseInt(params['id']);
+    });
 
+    this.productService.getProducts().subscribe((data) => {
+      this.product = data.filter(
+        (product: productProps) => product.id === this.id
+      )[0];
+    });
+  }
 }
