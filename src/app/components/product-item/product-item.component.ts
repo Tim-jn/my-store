9657cdca4da58740bfc1d446f36bfbdefaca74fa
Tim <Tim280696@hotmail.com>
2022-08-15 17:04:09 +0200

    Add addToCart + message confirmation

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { productProps } from '../product-list/product-list.component';
+import { CartService } from 'src/app/services/cart.service';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-product-item',
@@ -8,8 +10,19 @@ import { productProps } from '../product-list/product-list.component';
 })
 export class ProductItemComponent implements OnInit {
   @Input() product!: productProps;
+  quantity!: number;
+  selectQuantity: number = 1;
+  productAdded: boolean = false;
 
-  constructor() {}
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {}
+
+  addToCart(product: productProps) {
+    this.cartService.addToCart(product, Number(this.selectQuantity));
+    this.productAdded = true;
+    setTimeout(() => {
+      this.productAdded = false;
+    }, 1500);
+  }
 }
