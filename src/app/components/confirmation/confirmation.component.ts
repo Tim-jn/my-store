a5b098130e7b2b0d0e78714ee7ad633568102f37
Tim <Tim280696@hotmail.com>
2022-08-18 11:54:32 +0200

    Create confirmation component

--- src/app/components/confirmation/confirmation.component.ts
+++ src/app/components/confirmation/confirmation.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
-  styleUrls: ['./confirmation.component.css']
+  styleUrls: ['./confirmation.component.css'],
 })
 export class ConfirmationComponent implements OnInit {
+  name!: string;
+  value!: number;
 
-  constructor() { }
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
+    this.name = this.cartService.name;
+    this.value = this.cartService.value;
+    this.cartService.clearCart();
   }
-
 }
