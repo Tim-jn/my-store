cf9db87f66f1b716ff79ea1a8da9c389be516548
Tim <Tim280696@hotmail.com>
2022-08-18 09:53:41 +0200

    Add update quantity

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart.service';
+import { cart } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,14 +8,34 @@ import { CartService } from '../../services/cart.service';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  products = this.cartService.getItems();
+  cart: cart[] = [];
   total: number = 0;
+  cartQuantity!: number;
 
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
-    this.products.map((product) => {
-      this.total += product.price;
+    this.cart = this.cartService.getProducts();
+    // this.products.some((product) => {
+    //   if (this.products.includes(product)) {
+    //     return console.log(true);
+    //   }
+    //   return console.log(false);
+    // });
+    // this.quantityProducts = this.products.map((product) => {
+    //   return { ...product, quantity: this.selectQuantity };
+    // });
+    this.cart.map((cartItem) => {
+      this.cartQuantity = cartItem.quantity;
+      this.total += cartItem.product.price * cartItem.quantity;
     });
+
+    // }
+  }
+
+  handleUpdateQuantity() {
+    this.cartService.updateQuantity();
+    this.total = this.cartService.value;
+    console.log(this.cart);
   }
 }
